4d29a622634818d5d42c3b3aa45074cf6d1f8f5e
Andre <Andre.AsturiasGary@flemingcollege.ca>
2022-03-24 16:25:16 -0400

    Create PokemonPasteBin.py

---
+++ PokemonPasteBin.py
@@ -0,0 +1,22 @@
+""" 
+COMP 593 - Lab 6
+
+Description: 
+  Receives the name or number of a pokemon to look up information about it,
+  then posts its abilities to PasteBin specifying a title for the paste.
+
+Usage:
+  python PokemonPasteBin.py pokemon_name
+
+Parameters:
+  pokemon_name = Name or number of the pokemon to be searched on the PokeAPI.
+
+History:
+  Date        Author      Description
+  2022-03-24  A.Asturias  Initial creation
+"""
+
+def main():
+    return
+
+main()
